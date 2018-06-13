d8031b4332055443930c0475b383ae828a99b5a3
Serhii Plyhun <snuk188@gmail.com>
2018-06-13 18:33:36 +0200

    Initialization. Does not work yet.

--- src/lib_win32.rs
+++ src/lib_win32.rs
@@ -8,7 +8,9 @@ use plygui_win32::common;
 use winapi::shared::windef;
 use winapi::shared::minwindef;
 use winapi::um::winuser;
+use winapi::um::wingdi;
 use winapi::um::commctrl;
+use winapi::ctypes::c_void;
 
 use std::{ptr, mem};
 use std::os::windows::ffi::OsStrExt;
@@ -16,7 +18,7 @@ use std::ffi::OsStr;
 use std::cmp::max;
 
 lazy_static! {
-	pub static ref WINDOW_CLASS: Vec<u16> = OsStr::new("Image")
+	pub static ref WINDOW_CLASS: Vec<u16> = OsStr::new("STATIC")
         .encode_wide()
         .chain(Some(0).into_iter())
         .collect::<Vec<_>>();
@@ -28,16 +30,78 @@ pub type Image = Member<Control<ImageWin32>>;
 pub struct ImageWin32 {
     base: common::WindowsControlBase<Image>,
     
+    bmp: windef::HBITMAP,
     scale: super::ScalePolicy,
+}
+
+impl ImageWin32 {
+	fn install_image(&mut self, content: super::image::DynamicImage) {
+		use image::{Pixel, GenericImage};
+		
+		let (w, h) = content.dimensions();
+
+        let bminfo = wingdi::BITMAPINFO {
+            bmiHeader: wingdi::BITMAPINFOHEADER {
+                biSize: mem::size_of::<wingdi::BITMAPINFOHEADER>() as u32,
+                biWidth: w as i32,
+                biHeight: h as i32,
+                biPlanes: 1,
+                biBitCount: 32,
+                biCompression: wingdi::BI_RGB,
+                biSizeImage: 0,
+                biXPelsPerMeter: 0,
+                biYPelsPerMeter: 0,
+                biClrUsed: 0,
+                biClrImportant: 0,
+            },
+            bmiColors: unsafe { mem::zeroed() },
+		};
+        
+        unsafe {
+            let mut pv_image_bits = ptr::null_mut();
+            let hdc_screen = winuser::GetDC(ptr::null_mut());
+            self.bmp = wingdi::CreateDIBSection(hdc_screen,
+                                           &bminfo,
+                                           wingdi::DIB_RGB_COLORS,
+                                           &mut pv_image_bits,
+                                           ptr::null_mut(),
+                                           0);
+            winuser::ReleaseDC(ptr::null_mut(), hdc_screen);
+            if self.bmp.is_null() {
+		        panic!("Could not load image.")
+		    }
+
+            let cb_stride = w as u32 * 4;
+
+            for (x, y, p) in content.pixels() {
+                let mut p = p.to_rgba();
+                p.data = [p.data[2], p.data[1], p.data[0], p.data[3]];
+                *(pv_image_bits.offset(((x * 4) + ((h - y - 1) * cb_stride)) as isize) as *mut [u8; 4]) = p.data;
+            }
+        }
+	}
+	fn remove_image(&mut self) {
+		unsafe { wingdi::DeleteObject(self.bmp as *mut c_void); }
+		self.bmp = ptr::null_mut();
+	}
+}
+
+impl Drop for ImageWin32 {
+	fn drop(&mut self) {
+		self.remove_image();
+	}
 }
 
 impl image_dev::ImageInner for ImageWin32 {
 	fn with_content(content: super::image::DynamicImage) -> Box<super::Image> {
 		let mut i = Box::new(Member::with_inner(Control::with_inner(ImageWin32 {
 			base: common::WindowsControlBase::new(),
+			
+			bmp: ptr::null_mut(),
 			scale: super::ScalePolicy::Fit(layout::Gravity::Center),			
 		}, ()), MemberFunctions::new(_as_any, _as_any_mut, _as_member, _as_member_mut)));
 		
+		i.as_inner_mut().as_inner_mut().install_image(content);
 		i
 	}
 	fn set_scale(&mut self, base: &mut MemberControlBase, policy: super::ScalePolicy) {
@@ -53,10 +117,34 @@ impl image_dev::ImageInner for ImageWin32 {
 
 impl ControlInner for ImageWin32 {
 	fn on_added_to_container(&mut self, base: &mut MemberControlBase, parent: &controls::Container, x: i32, y: i32) {
-		
-	}
-    fn on_removed_from_container(&mut self, base: &mut MemberControlBase, parent: &controls::Container) {
-    	
+		let selfptr = base as *mut _ as *mut c_void;
+        let (pw, ph) = parent.draw_area_size();
+        //let (lp,tp,rp,bp) = self.base.layout.padding.into();
+        let (lm, tm, rm, bm) = base.control.layout.margin.into();
+        let (hwnd, id) = unsafe {
+            self.base.hwnd = parent.native_id() as windef::HWND; // required for measure, as we don't have own hwnd yet
+            let (w, h, _) = self.measure(base, pw, ph);
+            common::create_control_hwnd(
+                x as i32 + lm,
+                y as i32 + tm,
+                w as i32 - rm - lm,
+                h as i32 - bm - tm,
+                parent.native_id() as windef::HWND,
+                0,
+                WINDOW_CLASS.as_ptr(),
+                "",
+                winuser::SS_BITMAP | winuser::SS_CENTERIMAGE | winuser::WS_TABSTOP,
+                selfptr,
+                Some(handler),
+            )
+        };
+        self.base.hwnd = hwnd;
+        self.base.subclass_id = id;
+    }
+    fn on_removed_from_container(&mut self, _: &mut MemberControlBase, _: &controls::Container) {
+        common::destroy_hwnd(self.base.hwnd, self.base.subclass_id, Some(handler));
+        self.base.hwnd = 0 as windef::HWND;
+        self.base.subclass_id = 0;	
     }
     
     fn parent(&self) -> Option<&controls::Member> {
@@ -201,7 +289,48 @@ unsafe extern "system" fn handler(hwnd: windef::HWND, msg: minwindef::UINT, wpar
                 let mut sc2: &mut Image = mem::transmute(param);
                 (cb.as_mut())(sc2, width, height);
             }
-        }
+        },
+        winuser::WM_PAINT => {
+        	let sc = sc.as_inner_mut().as_inner_mut();
+        	let size = sc.size();
+        	
+	        let mut bm: wingdi::BITMAP = mem::zeroed();
+	        let mut ps: winuser::PAINTSTRUCT = mem::zeroed();
+	
+	        let mut hdc = winuser::BeginPaint(hwnd, &mut ps); //HDC
+	
+	        let mut hdc_mem = wingdi::CreateCompatibleDC(hdc); //HDC
+	        let hbm_old = wingdi::SelectObject(hdc_mem, sc.bmp as *mut c_void); //HBITMAP
+	
+	        wingdi::GetObjectW(sc.bmp as *mut c_void, mem::size_of::<wingdi::BITMAP>() as i32, &mut bm as *mut _ as *mut c_void);
+	
+	        //wingdi::BitBlt(hdc, 0, 0, bm.bmWidth, bm.bmHeight, hdc_mem, 0, 0, wingdi::SRCCOPY);
+	
+	        //wingdi::SelectObject(hdc_mem, hbm_old);
+	        
+	        let blendfunc = wingdi::BLENDFUNCTION {
+		        BlendOp: 0,
+		        BlendFlags: 0,
+		        SourceConstantAlpha: 255,
+		        AlphaFormat: 1,
+		    };
+		
+		    wingdi::GdiAlphaBlend(hdc,
+		                         0,
+		                         0,
+		                         size.0 as i32,
+		                         size.1 as i32,
+		                         hdc_mem,
+		                         0,
+		                         0,
+		                         500,
+		                         500,
+								blendfunc);
+	        
+	        wingdi::DeleteDC(hdc_mem);
+	
+	        winuser::EndPaint(hwnd, &ps);
+	    }
         _ => {}
     }
 
