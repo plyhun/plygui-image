79ba33c03539cfec10170e218650f15f5fef539b
admin <admin@192.168.128.129>
2018-09-14 22:15:58 +0200

    Cleanup

--- src/lib_cocoa.rs
+++ src/lib_cocoa.rs
@@ -148,27 +148,8 @@ impl HasLayoutInner for ImageCocoa {
 }
 
 impl Drawable for ImageCocoa {
-    fn draw(&mut self, member: &mut MemberBase, _control: &mut ControlBase, coords: Option<(i32, i32)>) {
-        if coords.is_some() {
-            self.base.coords = coords;
-        }
-        if let Some((x, y)) = self.base.coords {
-            let (_, ph) = self.parent_mut().unwrap().is_container_mut().unwrap().size();
-            unsafe {
-                let mut frame: NSRect = self.base.frame();
-                frame.size = NSSize::new((self.base.measured_size.0 as i32) as f64, (self.base.measured_size.1 as i32) as f64);
-                frame.origin = NSPoint::new(x as f64, (ph as i32 - y - self.base.measured_size.1 as i32) as f64);
-                let () = msg_send![self.base.control, setFrame: frame];
-            }
-            if let Some(ref mut cb) = member.handler_resize {
-                unsafe {
-                    let object: &Object = mem::transmute(self.base.control);
-                    let saved: *mut c_void = *object.get_ivar(common::IVAR);
-                    let mut ll2: &mut Image = mem::transmute(saved);
-                    (cb.as_mut())(ll2, self.base.measured_size.0, self.base.measured_size.1);
-                }
-            }
-        }
+    fn draw(&mut self, _member: &mut MemberBase, _control: &mut ControlBase, coords: Option<(i32, i32)>) {
+        self.base.draw(coords);
     }
     fn measure(&mut self, member: &mut MemberBase, control: &mut ControlBase, parent_width: u16, parent_height: u16) -> (u16, u16, bool) {
         use std::cmp::max;
